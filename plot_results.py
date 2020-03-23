9e7854986fff498497ba199f6be905647f64b7cd
petermaksymo <pmaksyma@gmail.com>
2020-03-23 14:00:13 -0400

    RGB implementation based off observation for car racing

--- plot_results.py
+++ plot_results.py
@@ -3,19 +3,19 @@ import matplotlib.pyplot as plt
 
 def plot():
     plt.figure()
-    durations = np.loadtxt('./training_results.csv', delimiter=',')
-    plt.title('CartPole training results')
+    durations = np.loadtxt('./racing_RGB_training_results.csv', delimiter=',')
+    plt.title('Car Racing training results')
     plt.xlabel('Episode')
     plt.ylabel('Duration')
-    plt.plot(durations[:250], label='Episode result')
+    plt.plot(durations[:], label='Episode result')
 
     # Take 100 episode averages and plot them too
-    mean = np.zeros(250)
-    for i in range(0, len(durations[:250])-100):
+    mean = np.zeros(durations.shape[0])
+    for i in range(0, len(durations)-100):
         mean[i+100] = np.mean(durations[i : i +100])
     plt.plot(mean, label='Mean for past 100 episodes')
 
-    plt.annotate(' Solved\nProblem', xy=(138, 195.5), xytext=(129, 450), arrowprops=dict(facecolor='black', headwidth=10, width=2))
+    # plt.annotate(' Solved\nProblem', xy=(138, 195.5), xytext=(129, 450), arrowprops=dict(facecolor='black', headwidth=10, width=2))
     plt.legend()
     plt.show()
 
